c3b35b148c8bdb46d6e81031f1e1cc3dd71e34ff
jcollyer <collyerdesign@gmail.com>
2015-06-13 11:31:24 -0700

    add description to table

--- src/main.jsx
+++ src/main.jsx
@@ -7,39 +7,35 @@ require('./style.less');
 var Domain = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function() {
-    return {items: [], text: '', show_help: false};
+    return {items: [], domain: '', text: '', show_help: false};
   },
-  onChange: function(e) {
-    this.setState({text: e.target.value, show_help: false});
+  onDomainChange: function(e) {
+    this.setState({domain: e.target.value, show_help: false});
+  },
+  onTextChange: function(e) {
+    this.setState({text: e.target.value});
   },
   validDomain: function(value) {
     return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
   },
   handleSubmit: function(e) {
-    var domain = this.state.text;
+    var domain = this.state.domain;
 
     if (this.validDomain(domain)){
       e.preventDefault();
       this.firebaseRefs.items.push({
-        text: domain
+        domain: domain,
+        text: this.state.text
       });
-      this.setState({text: "", show_help: false});
+      this.setState({domain: "", text: "", show_help: false});
     } else {
       e.preventDefault();
-      this.setState({text: "", show_help: true});
+      this.setState({domain: "", show_help: true});
     }
   },
   componentWillMount: function() {
     var ref = new Firebase("https://burning-heat-3182.firebaseio.com/items/");
-      this.bindAsArray(ref, "items");
-    // this.bindAsObject(new Firebase("https://burning-heat-3182.firebaseio.com/items/"), "items");
-    // this.firebaseRef = new Firebase("https://burning-heat-3182.firebaseio.com/items/");
-    //  this.firebaseRef.on("child_added", function(dataSnapshot) {
-    //    this.items.push(dataSnapshot.val());
-    //    this.setState({
-    //      items: this.items
-    //    });
-     // }.bind(this));
+    this.bindAsArray(ref, "items");
   },
   componentWillUnmount: function() {
     this.firebaseRef.off();
@@ -51,7 +47,12 @@ var Domain = React.createClass({
         <h3>Domain</h3>
         <DomainList items={this.state.items} />
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
+          domain:
+          <input onChange={this.onDomainChange} value={this.state.domain} />
+          <br />
+          description:
+          <input type="text" onChange={this.onTextChange} value={this.state.text} />
+          <br />
           <button>{'Add Domain #' + (this.state.items.length + 1)}</button>
         </form>
       </div>
